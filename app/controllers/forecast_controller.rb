2034416480896acdbaa16ca69f5b09621940d54a
dmoriarb <dmoriarb@chicagobooth.edu>
2016-10-22 10:20:18 -0500

    Finished project
    FINISHED

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,19 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+coord=@lat+","+@lng
+url2 = "https://api.darksky.net/forecast/edf7ebf38240e9a09c2f3abcbdb81c52/"+coord
+parsed_dati = JSON.parse(open(url2).read)
 
+    @current_temperature = parsed_dati["currently"]["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary = parsed_dati["currently"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_dati["minutely"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_dati["hourly"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_dati["daily"]["summary"]
 
     render("forecast/coords_to_weather.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -17,17 +17,23 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the variable @street_address_without_spaces.
     # ==========================================================================
 
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address="+@street_address_without_spaces
+    parsed_data = JSON.parse(open(url).read)
+    lat = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    lng = parsed_data["results"][0]["geometry"]["location"]["lng"]
+coord= [lat,lng].to_sentence(two_words_connector: ',')
+    url2 = "https://api.darksky.net/forecast/edf7ebf38240e9a09c2f3abcbdb81c52/"+coord
+    parsed_dati = JSON.parse(open(url2).read)
 
+    @current_temperature = parsed_dati["currently"]["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary = parsed_dati["currently"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_dati["minutely"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_dati["hourly"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_dati["daily"]["summary"]
 
     render("meteorologist/street_to_weather.html.erb")
   end
